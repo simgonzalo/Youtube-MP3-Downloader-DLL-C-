cdd1b114071f1a8fa365698b67a1d73f303542a7
Gonzalo <Gonzalo@GPC>
2016-09-09 17:51:59 +0200

    Agregar archivos de proyecto.

---
+++ Youtube MP3 Downloader/Main.cs
@@ -0,0 +1,10 @@
+﻿namespace Youtube_MP3_Downloader
+{
+    public class Main
+    {
+        public static string get_Download_link(string youtube_Url)
+        {
+            return global.download_link("http://www.youtubeinmp3.com/widget/button/?color=ba1717&amp;video=" + youtube_Url);
+        }
+    }
+}
---
+++ Youtube MP3 Downloader/global.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Youtube_MP3_Downloader
+    {
+        internal class global
+            {
+                public static string gethtml(string url)
+                    {
+                        var urlAddress = url;
+                        var request = (HttpWebRequest) WebRequest.Create(urlAddress);
+                        var response = (HttpWebResponse) request.GetResponse();
+                        var data = "";
+                        if(response.StatusCode == HttpStatusCode.OK)
+                            {
+                                var receiveStream = response.GetResponseStream();
+                                StreamReader readStream = null;
+                                if(response.CharacterSet == null)
+                                    {
+                                        readStream = new StreamReader(receiveStream);
+                                    }
+                                else
+                                    {
+                                        readStream = new StreamReader(receiveStream,
+                                            Encoding.GetEncoding(response.CharacterSet));
+                                    }
+                                data = readStream.ReadToEnd();
+                                response.Close();
+                                readStream.Close();
+                            }
+                        return data;
+                    }
+
+                public static string download_link(string youtube_url)
+                    {
+                        var a = gethtml(youtube_url);
+                        var html = gethtml(youtube_url);
+                        var reg = Regex.Split(html, " ");
+                        var url = reg [14];
+                        url = url.Replace("href =\"/download/get/", "");
+                        url = url.Replace("\"", "");
+                        url = url.Replace("href=\"/download/get/?i=", "");
+                        url = url.Replace("href=/download/get/", "");
+                        var start = "http://www.youtubeinmp3.com/download/get/";
+                        var final_url = start + url;
+                        return final_url;
+                    }
+            }
+    }
